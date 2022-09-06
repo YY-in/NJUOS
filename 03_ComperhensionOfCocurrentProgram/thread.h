82e654cd7514c0e581661f0859aac80411ee3996
YY-in <1398035515@qq.com>
2022-09-06 13:07:51 +0800

    add：comment of thread.h

--- 03_ComperhensionOfCocurrentProgram/thread.h
+++ 03_ComperhensionOfCocurrentProgram/thread.h
@@ -8,6 +8,7 @@
 
 #define NTHREAD 64
 enum { T_FREE = 0, T_LIVE, T_DEAD, };
+
 struct thread {
   int id, status;
   pthread_t thread;
@@ -31,16 +32,16 @@ void create(void *fn) {
     .entry = fn,
   };
   /**
-   *int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg); 
-   1. 线程句柄 thread：当一个新的线程调用成功之后，就会通过这个参数将线程的句柄返回给调用者，以便对这个线程进行管理。
-   2. 入口函数 start_routine()： 当你的程序调用了这个接口之后，就会产生一个线程，而这个线程的入口函数就是start_routine()。
-      如果线程创建成功，这个接口会返回0。
-   3. 入口函数参数 *arg : start_routine()函数有一个参数，这个参数就是pthread_create的最后一个参数arg。
-      这种设计可以在线程创建之前就帮它准备好一些专有数据，最典型的用法就是使用C++编程时的this指针。
+   * @brief 
+   * 
+   * @param thread 当一个新的线程调用成功之后，就会通过这个参数将线程的句柄返回给调用者，以便对这个线程进行管理。
+   * @param attr 指定线程所拥有的属性，当不需要修改线程的默认属性时，给它传递NULL就行
+   * @param start_routine 函数指针， 当你的程序调用了这个接口之后，就会产生一个线程，而这个线程的入口函数就是start_routine()。如果线程创建成功，这个接口会返回void*
+   * @param arg start_routine()函数有一个参数，这个参数就是pthread_create的最后一个参数arg。
       start_routine()有一个返回值，这个返回值可以通过pthread_join()接口获得。
-   4. 线程属性 attr： pthread_create()接口的第二个参数用于设置线程的属性。
-      这个参数是可选的，当不需要修改线程的默认属性时，给它传递NULL就行。具体线程有那些属性，我们后面再做介绍。
-   */
+   * @return * int 
+   *int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg); 
+   */        
   pthread_create(&(tptr->thread), NULL, wrapper, tptr);
   ++tptr;
 }
@@ -49,6 +50,12 @@ void join() {
   for (int i = 0; i < NTHREAD; i++) {
     struct thread *t = &tpool[i];
     if (t->status == T_LIVE) {
+      /**
+       * 线程的合并是一种主动回收线程资源的方案。当一个进程或线程调用了针对其它线程的pthread_join()接口，就是线程合并了。
+       * 这个接口会阻塞调用进程或线程，直到被合并的线程结束为止。当被合并线程结束，pthread_join()接口就会回收这个线程的资源，并将这个线程的返回值返回给合并者。
+       * @param thread 代表需要你等待的线程
+       * @param value_ptr 是一个指针，指向线程的返回值
+       */
       pthread_join(t->thread, NULL);
       t->status = T_DEAD;
     }
